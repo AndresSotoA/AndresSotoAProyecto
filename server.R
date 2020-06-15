97d6aa173b84b1d044985e7f237d9dc043fcefa5
AndresSotoA <33590528+AndresSotoA@users.noreply.github.com>
2020-06-15 00:16:22 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,26 +1,15 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
-library(ggplot2)
-library(dplyr)
-# Define server logic required to draw a histogram
+# Define server logic required to draw 
 shinyServer(function(input, output) {
-
+  
     output$Segmentos_Plot <- renderPlot({
 
-          datos <- iris4 %>% select(x = input$xvar, y = input$yvar)
+          datos <- iris[,1:4] %>% select(x = input$xvar, y = input$yvar)
           modelo <- kmeans(datos, centers = input$Segmentos, nstart = 25)
           datos1 <- cbind(datos, grupo= modelo$cluster)
           
           
-        # draw the histogram with the specified number of bins
+        
       p <- ggplot(datos1, aes(x = x, y =  y, color = factor(grupo)))+
         geom_point()
       p + labs(x = input$xvar , y = input$yvar)
